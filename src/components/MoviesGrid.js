e6e80c05ffc4d1e4a82879b05d8a41739d15a869
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2025-02-18 00:34:25 +0530

    Search bar implementation

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,28 +1,27 @@
-import React , {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import '../styles.css'
+import MovieCard from "./movieCard";
 
 export default function MoviesGrid() {
 
     const [movies, setMovies] = useState([]);
+//for searching the movies
 
+const [searchTerm , setSearchTerm] = useState("");
     //useffect with fetching the data from movies.json
-    useEffect(() => {  
-        fetch("movies.json").then((response) => response.json()).then((data) => setMovies(data)); 
-     }, []);
-    
+    useEffect(() => {
+        fetch("movies.json").then((response) => response.json()).then((data) => setMovies(data));
+    }, []);
+
     return (
-<div className="movies-grid">
-    {movies.map(movie=>(
-        <div key={movie.id} className="movie-card">
-            <img src={`images/${movie.image}`} alt={movie.title}/>
-            <div className="movie-card-info">
-                <h3 className="movie-card-title">{movie.title}</h3>
-                <p className="movie-card-genre">{movie.genre}</p>
-                <p className="movie-card-rating">{movie.rating}</p>
-            </div>
-        </div>    
-    ))
-    }
-</div>
+       <div>
+        <input className="search-input" type="text" placeholder="Search..." />
+         <div className="movies-grid">
+            {movies.map(movie => (
+                <MovieCard movie={movie}></MovieCard>
+            ))
+            }
+        </div>
+       </div>
     );
 }
---
+++ src/components/movieCard.js
@@ -0,0 +1,34 @@
+import React , {useState, useEffect} from "react";
+import '../styles.css'
+
+export default function MovieCard({movie}) {
+//handling the errors
+    const handleErrors = (e) => {
+        e.target.src = 'images/default.jpg'
+    }
+
+
+    //rating color 
+    const getRatingcolor = (rating) => {
+        if (rating >= 8) {
+            return "rating-good";
+        } else if (rating >= 6) {
+            return "rating-ok";
+        } else {
+            return "rating-bad";
+        }   
+    };
+
+    
+   return (
+    <div key={movie.id} className="movie-card">
+            <img src={`images/${movie.image}`} alt={movie.title} onError={handleErrors}
+            />
+            <div className="movie-card-info">
+                <h3 className="movie-card-title">{movie.title}</h3>
+                <p className="movie-card-genre">{movie.genre}</p>
+                <p className={`movie-card-rating ${getRatingcolor(movie.rating)}`}>{movie.rating}</p>
+            </div>
+        </div>    
+   )
+}
