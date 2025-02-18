07901b1d106876e7222852d06db67f89f4bcdf01
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2025-02-18 09:45:13 +0530

    intialise router

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -8,6 +8,34 @@ export default function MoviesGrid() {
 //for searching the movies
 
 const [searchTerm , setSearchTerm] = useState("");
+
+const [genre, setGenre] = useState("All Genre");
+const [rating, setRatings] = useState("All Ratings");
+const filteredMoviesByGenre = (e) => {
+    setGenre(e.target.value)
+}
+const filteredMoviesByRating = (e) => {
+    setRatings(e.target.value)
+}
+const handleSearch = (e) => {
+    setSearchTerm(e.target.value)
+}
+
+const matchesGenre = (movie, genre)=>{
+    return genre === "All Genre" || movie.genre.toLowerCase === genre;
+};
+
+const matchesRating = (movie, rating) => {
+    return rating === "All Ratings" || movie.rating >= rating;
+};
+
+const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+}
+const filteredMovies = movies.filter((movie) => 
+    matchesGenre(movie, genre)  && matchesSearchTerm(movie, searchTerm)
+);
+
     //useffect with fetching the data from movies.json
     useEffect(() => {
         fetch("movies.json").then((response) => response.json()).then((data) => setMovies(data));
@@ -15,9 +43,28 @@ const [searchTerm , setSearchTerm] = useState("");
 
     return (
        <div>
-        <input className="search-input" type="text" placeholder="Search..." />
+        <input className="search-input" type="text" placeholder="Search..." value={searchTerm} onChange={handleSearch}/>
+        <div className="filter-bar" >
+            <div className="filter-slot">
+            <label>Genre</label>
+            <select className="filter-dropdown" value={genre} onChange={filteredMoviesByGenre}>
+                <option>All Genre</option>
+                <option>Action</option>
+                <option>Horror</option>
+                <option>fantasy</option>
+            </select>
+        </div>
+        <div className="filter-slot">
+            <label >Ratings</label>
+            <select className="filter-dropdown" value={rating} onChange={filteredMoviesByRating}>
+                <option>All Ratings</option>
+                <option>7.7</option>
+                <option>6</option>
+            </select>
+            </div>
+        </div>
          <div className="movies-grid">
-            {movies.map(movie => (
+            {filteredMovies.map(movie => (
                 <MovieCard movie={movie}></MovieCard>
             ))
             }
--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,4 +1,4 @@
-import React , {useState, useEffect} from "react";
+import React from "react";
 import '../styles.css'
 
 export default function MovieCard({movie}) {
@@ -19,7 +19,7 @@ export default function MovieCard({movie}) {
         }   
     };
 
-    
+
    return (
     <div key={movie.id} className="movie-card">
             <img src={`images/${movie.image}`} alt={movie.title} onError={handleErrors}
